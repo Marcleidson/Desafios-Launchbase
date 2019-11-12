4bdfc31d78daf129d17a6c9acf40ded6ea1ae657
Marcleidson <47360687+Marcleidson@users.noreply.github.com>
2019-11-12 14:13:19 -0300

    Create aposentadoria.js

---
+++ aposentadoria.js
@@ -0,0 +1,14 @@
+const nome = 'Marcleidson'
+const sexo = 'M'
+const idade = 35
+const contribuicao = 18
+
+const regra2 = idade + contribuicao
+
+
+if ((sexo == 'M' && contribuicao >= 35) || (sexo == 'F' && contribuicao >= 30)) {
+  if ((sexo == 'M' && regra2 >= 95) || (sexo == 'F' && regra2 >= 85) {
+  console.log (`${nome}, você pode se aposentar.`
+  } else {
+  console.log (`${nome}, você não pode se aposentar.`)
+}
