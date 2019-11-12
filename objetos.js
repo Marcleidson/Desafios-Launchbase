9e9ae1d6d03df1d948039cdaf983ed3d23ca614f
Marcleidson <47360687+Marcleidson@users.noreply.github.com>
2019-11-12 16:11:25 -0300

    criando objetos
    criando objetos

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const empresa = {
+    nome: 'Marcleidson',
+    cor: 'Roxo',
+    foco: 'Programação',
+    endereco: {
+        rua: 'Rua São Paulo',
+        numero: 260
+    }
+}
+console.log (`A empresa ${empresa.nome}, está localizada em ${empresa.endereco.rua}, nº ${empresa.endereco.numero}`)
